1439837b6c777b7bf353cb846a9a5d02aaf4190e
Nguyen The Thao <nguyenthethaok44kd01@gmail.com>
2020-12-25 15:51:04 +0700

    Add routers

--- main.go
+++ main.go
@@ -1,31 +1,12 @@
 package main
 
 import (
-	"fire_heart/controllers"
 	"fire_heart/models/db"
-	"github.com/gin-gonic/gin"
+	"fire_heart/routers"
 )
 
 func main() {
 	db.Connection()
-	userController := new (controllers.UserController)
-	router := gin.Default()
-	router.POST("/", userController.Store)
-	router.GET("/:email", userController.Show)
-	router.DELETE("/:email", userController.Delete)
-	//client := db.Client.Database("fire_heart").Collection("posts")
-	//filter := bson.D
-	//type Post struct {
-	//
-	//	Title string `json:"title,omitempty""`
-	//
-	//	Body string `json:"body,omitempty""`
-	//
-	//}
-	//
-	//var post Post
-
-	//fmt.Println(*client)
-
+	router := routers.InitRouter()
 	_ = router.Run()
 }
--- routers/index.go
+++ routers/index.go
@@ -1,1 +1,23 @@
 package routers
+
+import (
+	"fire_heart/controllers"
+	"github.com/gin-gonic/gin"
+)
+
+func setAuthRoute(router *gin.Engine)  {
+	userController := new (controllers.UserController)
+	router.POST("/", userController.Store)
+	router.GET("/:email", userController.Show)
+	router.DELETE("/:email", userController.Delete)
+}
+
+func InitRouter() *gin.Engine {
+	router := gin.Default()
+	router.Use(gin.Logger())
+	router.Use(gin.Recovery())
+
+	setAuthRoute(router)
+
+	return router
+}
