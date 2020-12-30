a78037cf45e477bc78f94b8792b5218f45fb6e50
Nguyen The Thao <nguyenthethaok44kd01@gmail.com>
2020-12-30 11:29:33 +0700

    Fix env for heroku

--- utils/index.go
+++ utils/index.go
@@ -2,15 +2,15 @@ package utils
 
 import (
 	"github.com/joho/godotenv"
-	"log"
 	"os"
 )
 
 func Env(key string) string {
 	err := godotenv.Load()
 	if err != nil {
-		log.Fatal("Error loading .env file")
-		return ""
+		// for heroku
+		return os.Getenv(key)
+		//log.Fatal("Error loading .env file")
 	}
 
 	return os.Getenv(key)
